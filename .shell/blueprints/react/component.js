d6898fe6af4cad7dce8611dae762c9ec2db7a567
Bilo Lwabona <bilo.lwabona@gmail.com>
2017-10-16 11:40:37 +0200

    added lifecycle to react.component blueprint

--- .shell/blueprints/react/component.js
+++ .shell/blueprints/react/component.js
@@ -8,9 +8,23 @@ export default class MyComponent extends Component {
         index: PropTypes.number.isRequired,
         name: PropTypes.string.isRequired
     }
+    //#region Lifecycle
+    // https://reactjs.org/docs/react-component.html#componentwillreceiveprops
+    // mounting
     constructor(props) {
         super(props)
     }
+    componentWillMount() {}
+    componentDidMount() {}
+    // updates
+    componentWillReceiveProps(nextProps) {}
+    shouldComponentUpdate(nextProps, nextState) {}
+    componentDidWillUpdate(nextProps, nextState) {}
+    componentDidUpdate(prevProps, prevState) {}
+    // unmounting & error handling
+    componentWillUnMount() {}
+    componentDidCatch(error, info) {}
+    //#endregion
     render() {
         return (
             <div className='page'>
